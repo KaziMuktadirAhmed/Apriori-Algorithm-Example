ec0b6c3f7dd8559db89eb16c9ff744003584a6d9
Kazi Muktadir Ahmed <55475124+KaziMuktadirAhmed@users.noreply.github.com>
2022-04-12 15:26:22 +0600

    created a simple Record.java for handling input transaction

--- src/DataProcessor/Record.java
+++ src/DataProcessor/Record.java
@@ -1,4 +1,34 @@
 package DataProcessor;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Record {
+    public final ArrayList<Integer> items;
+
+    public Record(ArrayList<Integer> items) {
+        this.items = items;
+    }
+
+    public boolean has (Integer targetItem) {
+        boolean isInside = false;
+        for (Integer itm : items) {
+            if (Objects.equals(itm, targetItem)) {
+                isInside = true;
+                break;
+            }
+        }
+        return  isInside;
+    }
+
+    public boolean has (ArrayList<Integer> targetItems) {
+        boolean isInside = true;
+        for (Integer itm : targetItems) {
+            if(!has(itm)) {
+                isInside = false;
+                break;
+            }
+        }
+        return isInside;
+    }
 }
