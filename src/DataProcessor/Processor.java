abfa95517d5079bbcdb824c82a127ffddcda5a82
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-22 19:53:57 +0600

    Merge remote-tracking branch 'origin/master'

--- src/DataProcessor/Processor.java
+++ src/DataProcessor/Processor.java
@@ -3,8 +3,13 @@ package DataProcessor;
 import java.util.ArrayList;
 
 public class Processor {
-    private ArrayList<Record> oneLengthRecords;
+    private final int min_support;
+
+    private final ArrayList<Record> oneLengthRecords;
     private ArrayList<Record> twoLengthRecords;
 
-
+    public Processor(int min_support, ArrayList<Record> oneLengthRecords) {
+        this.min_support = min_support;
+        this.oneLengthRecords = oneLengthRecords;
+    }
 }
