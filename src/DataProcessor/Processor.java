1618fea2c70af0cda40ed9672d2944653b1d4ad7
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-23 03:03:54 +0600

    Merge remote-tracking branch 'origin/master'

--- src/DataProcessor/Processor.java
+++ src/DataProcessor/Processor.java
@@ -8,6 +8,7 @@ public class Processor {
     private final int min_support_count;
 
     private Input input_processor;
+    private final HashMap<Integer, ArrayList<Record>> support_counts_without_min;
     private final HashMap<Integer, ArrayList<Record>> support_counts;
 
     private int total_data_count;
@@ -15,6 +16,7 @@ public class Processor {
     public Processor(int min_support_count) {
         this.min_support_count = min_support_count;
         this.support_counts = new HashMap<>();
+        this.support_counts_without_min = new HashMap<>();
     }
 
     public void configure (String filePath) throws FileNotFoundException {
@@ -22,35 +24,65 @@ public class Processor {
         this.total_data_count = input_processor.databaseSize();
         support_counts.put(1, input_processor.get_one_len_rec());
 
-        for (Record rec: support_counts.get(1)) {
+        ArrayList<Record> two_len_recs = generate_next_gen(support_counts.get(1));
+        support_counts.put(2, two_len_recs);
+
+        ArrayList<Record> three_len_recs = generate_next_gen(two_len_recs);
+
+        ArrayList<Record> four_len_recs = generate_next_gen(three_len_recs);
+
+        for (Record rec: four_len_recs) {
             rec.print();
         }
     }
 
-    private void generate_next_gen (ArrayList<Record> prev_gen) {
+    private ArrayList<Record> generate_next_gen (ArrayList<Record> prev_gen) {
         ArrayList<Record> next_gen = new ArrayList<>();
 
-        Record rec;
+        for(int i=0; i<prev_gen.size(); i++) {
+            Record rec;
 
-        for(int i=0; i< prev_gen.size(); i++) {
             for(int j=i+1; j< prev_gen.size(); j++) {
-//                prev_gen.get(i)
-                
+                ArrayList<Integer> uncommon = find_uncommon(prev_gen.get(i), prev_gen.get(j));
 
+                for (Integer num: uncommon) {
+                    ArrayList<Integer> temp_rec = new ArrayList<>(prev_gen.get(i).items);
+                    temp_rec.add(num);
+
+                    if(!find_record(temp_rec, next_gen)) {
+                        rec = new Record("hello" + i + "prev", temp_rec);
+                        rec.count = input_processor.count_record(rec);
+
+                        if(rec.count >= min_support_count) {
+                            next_gen.add(rec);
+                        }
+                    }
+                }
             }
         }
+
+        return next_gen;
     }
 
-    private boolean find_record (Record rec, ArrayList<Record> rec_list) {
+    private boolean find_record (ArrayList<Integer> temp_rec, ArrayList<Record> rec_list) {
         boolean isFound = false;
         if(rec_list.size() == 0) return false;
         for (Record r: rec_list) {
-            if(r.match(rec.items)){
+            if(r.match(temp_rec)){
                 isFound = true;
                 break;
             }
         }
         return isFound;
+    }
+
+    private ArrayList<Integer> find_uncommon (Record r1, Record r2) {
+        ArrayList<Integer> uncommon = new ArrayList<>();
+        for (Integer itm: r2.items) {
+            if(!r1.has(itm))
+                uncommon.add(itm);
+        }
+        return uncommon;
     }
 
 }
