db31c3ca0cbcd9718b1b25679a71fafece925115
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-04-22 23:29:50 +0600

    Merge remote-tracking branch 'origin/master'

--- src/DataProcessor/Processor.java
+++ src/DataProcessor/Processor.java
@@ -2,24 +2,26 @@ package DataProcessor;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Processor {
     private final int min_support_count;
 
     private Input input_processor;
-    private ArrayList<Record> oneLengthRecords;
-    private ArrayList<Record> twoLengthRecords;
+    private HashMap<Integer, ArrayList<Record>> support_counts;
 
     private int total_data_count;
 
     public Processor(int min_support_count) {
         this.min_support_count = min_support_count;
+        this.support_counts = new HashMap<>();
     }
 
     public void configure (String filePath) throws FileNotFoundException {
         this.input_processor = new Input(filePath);
-        this.oneLengthRecords = input_processor.get_one_len_rec();
         this.total_data_count = input_processor.databaseSize();
+        support_counts.put(1, input_processor.get_one_len_rec());
+
     }
 
 
